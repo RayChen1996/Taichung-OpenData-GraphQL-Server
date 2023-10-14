35c2181d65b346ac7afa7cbe946ede08e8c94a99
RayChen <ray.10315332@gmail.com>
2023-10-14 23:52:09 +0800

    移除 mongoose

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ const { BooksResult } = require("./schema/resolvers/BooksResult");
 const { FilterBook } = require("./schema/resolvers/FilterBook");
 
 const typeDefs = require("./schema/type");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 require("dotenv").config();
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const connection = mongoose.connection;
+// const connection = mongoose.connection;
 
-connection.once("open", () => {
-  console.log("mogoose connect!!!");
-});
+// connection.once("open", () => {
+//   console.log("mogoose connect!!!");
+// });
 
 const server = new ApolloServer({
   typeDefs,
